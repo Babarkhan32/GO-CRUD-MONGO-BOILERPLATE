8ec37b809dce174f5be26167c78db105e602b49b
babarkhan32 <babar.0khan1@gmail.com>
2023-01-17 22:46:45 +0500

    Added middleware-functions for JWT authentication and generation

--- contorllers/auth.controller.go
+++ contorllers/auth.controller.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"net/http"
 
+	"exmaple.com/job-x-apis/helpers"
 	"exmaple.com/job-x-apis/models"
 	"exmaple.com/job-x-apis/services"
 	"github.com/gin-gonic/gin"
@@ -29,11 +30,26 @@ func (uc *AuthController) Login(ctx *gin.Context) {
 		ctx.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
 		return
 	}
-	ctx.JSON(http.StatusCreated, gin.H{"data": user})
+	token, err := helpers.CreateJWT(user.Email)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+		return
+
+	}
+
+	ctx.JSON(http.StatusCreated, gin.H{"data": user, "token": token})
+
+}
+
+func (uc *AuthController) CheckStatus(ctx *gin.Context) {
+	message := uc.AuthService.CheckStatus()
+	ctx.JSON(http.StatusOK, gin.H{"message": message})
+
 }
 
 func (uc *AuthController) RegisterAuthRoutes(rg *gin.RouterGroup) {
 	authRoute := rg.Group("/auth")
+	
 	authRoute.POST("/login", uc.Login)
-
+	authRoute.GET("/check-status",helpers.ValidateJWT,uc.CheckStatus)
 }
---
+++ helpers/authHelper.go
@@ -0,0 +1,46 @@
+package helpers
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+)
+
+var secret = []byte("secret")
+
+func CreateJWT(username string) (string , error) {
+    token := jwt.New(jwt.SigningMethodHS256)
+
+    claims := token.Claims.(jwt.MapClaims)
+    claims["sub"] = username
+    claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
+
+    tokenString, err := token.SignedString(secret)
+    if err != nil {
+		message := ""
+        return message, err
+    }
+
+    return tokenString, nil
+}
+
+func ValidateJWT(c *gin.Context) {
+    tokenString := c.GetHeader("Authorization")
+    token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+        if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+            return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+        }
+        return secret, nil
+    })
+
+    if err != nil || !token.Valid {
+        c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+        return
+    }
+
+    c.Next()
+}
+
