e74e2dbc7be37b0b1af39de0abee5718dd8b9dc7
babarkhan32 <babar.0khan1@gmail.com>
2023-01-19 19:04:48 +0500

    Added jwt-key via env

--- helpers/authHelper.go
+++ helpers/authHelper.go
@@ -3,44 +3,45 @@ package helpers
 import (
 	"fmt"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 )
 
-var secret = []byte("secret")
+var secret = []byte(os.Getenv("JWT_SECRET"))
 
-func CreateJWT(username string) (string , error) {
-    token := jwt.New(jwt.SigningMethodHS256)
+func CreateJWT(username string) (string, error) {
+	token := jwt.New(jwt.SigningMethodHS256)
 
-    claims := token.Claims.(jwt.MapClaims)
-    claims["sub"] = username
-    claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
+	claims := token.Claims.(jwt.MapClaims)
+	claims["sub"] = username
+	claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
 
-    tokenString, err := token.SignedString(secret)
-    if err != nil {
+	tokenString, err := token.SignedString(secret)
+	if err != nil {
 		message := ""
-        return message, err
-    }
+		return message, err
+	}
 
-    return tokenString, nil
+	return tokenString, nil
 }
 
 func ValidateJWT(c *gin.Context) {
-    tokenString := c.GetHeader("Authorization")
-    token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-        if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-            return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
-        }
-        return secret, nil
-    })
+	tokenString := c.GetHeader("Authorization")
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("nexpected signing method: %v", token.Header["alg"])
+		}
+		return secret, nil
+	})
 
-    if err != nil || !token.Valid {
-        c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
-        return
-    }
+	if err != nil || !token.Valid {
+		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+		return
+	}
 
-    c.Next()
+	c.Next()
 }
 
